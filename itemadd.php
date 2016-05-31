ab112c63b3819781940747d67b2d07eafc4237b7
Ahmet Yilmaz <ahmet-uenal@hotmail.de>
2016-05-31 22:46:33 +0200

    Update itemadd.php

--- itemadd.php
+++ itemadd.php
@@ -1,9 +1,9 @@
 <?php
 //connecting to the database
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'itemlist');
-define('DB_USER','root');
-define('DB_PASSWORD','');
+define('DB_HOST', 'eu-cdbr-west-01.cleardb.com');
+define('DB_NAME', 'heroku_1ed81913353afff');
+define('DB_USER','b16f3dd482fc76');
+define('DB_PASSWORD','d96171c8');
 
 $con=mysql_connect(DB_HOST,DB_USER,DB_PASSWORD) or die("Failed to connect to MySQL: " . mysql_error());
 $db=mysql_select_db(DB_NAME,$con) or die("Failed to connect to MySQL: " . mysql_error());
