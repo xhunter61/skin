7c13ca23b8e50b6de5e1a5cf9427fac2bd13bba1
Ahmet Yilmaz <ahmet-uenal@hotmail.de>
2016-05-31 22:54:25 +0200

    Update itemshow.php

--- itemshow.php
+++ itemshow.php
@@ -2,10 +2,10 @@
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
 
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'itemlist');
-define('DB_USER','root');
-define('DB_PASSWORD','');
+define('DB_HOST', 'eu-cdbr-west-01.cleardb.com');
+define('DB_NAME', 'heroku_1ed81913353afff');
+define('DB_USER','b16f3dd482fc76');
+define('DB_PASSWORD','d96171c8');
 
 $con=mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD) or die("Failed to connect to MySQL: " . mysqli_error());
 $db=mysqli_select_db($con,DB_NAME) or die("Failed to connect to MySQL: " . mysqli_error());
