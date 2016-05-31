7cde81671be2d447959f0305adbcf2bc57b5ec4b
Ahmet Yilmaz <ahmet-uenal@hotmail.de>
2016-05-31 22:50:20 +0200

    Update itemadd.php

--- itemadd.php
+++ itemadd.php
@@ -5,8 +5,8 @@ define('DB_NAME', 'heroku_1ed81913353afff');
 define('DB_USER','b16f3dd482fc76');
 define('DB_PASSWORD','d96171c8');
 
-$con=mysql_connect(DB_HOST,DB_USER,DB_PASSWORD) or die("Failed to connect to MySQL: " . mysql_error());
-$db=mysql_select_db(DB_NAME,$con) or die("Failed to connect to MySQL: " . mysql_error());
+$con=mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD) or die("Failed to connect to MySQL: " . mysql_error());
+$db=mysqli_select_db($con,DB_NAME) or die("Failed to connect to MySQL: " . mysql_error());
 //inserting Record to the database
 $itemname = $_POST['itemname'];
 $kondition = $_POST['kondition'];
@@ -18,14 +18,14 @@ $itemid =  $_POST['itemid'];
 
 
 $query = "INSERT INTO `itemlist`(`Itemname`, `Kondition`, `Imagelink`, `Price`, `Steamprice`, `Endtime`) VALUES ('$itemname','$kondition','$imglink','$price','$steamprice','$endtime')";
-$result = mysql_query($query);
+$result = mysqli_query($con,$query);
 if($result)
 	{
 	    echo "Successfully updated database";
 	}
 	else
 	{
-	 die('Error: '.mysql_error($con));
+
 	}
-	mysql_close($con);
+	mysqli_close($con);
 ?>
