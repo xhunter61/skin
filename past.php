94b8086d78191a51891ca171b67e11dee0f3d094
Ahmet Yilmaz <ahmet-uenal@hotmail.de>
2016-05-31 22:53:28 +0200

    Update past.php

--- past.php
+++ past.php
@@ -115,10 +115,10 @@
 	}  else {
         include ('steamauth/userInfo.php');
         echo "<li class='dropdown' id='steam' title='".$steamprofile['avatarmedium']."' name='".$steamprofile['steamid']."'><a href='#'class='dropdown-toggle' data-toggle='dropdown' role='button' aria-haspopup='true' aria-expanded='false' style='padding-top:0px;padding-bottom:0px;line-height:50px'><img src='".$steamprofile['avatar'].">'><span class='caret'></span></a><ul id='profilenav' class='dropdown-menu'><li><a href='?logout'>Logout</a></li></ul></li>";
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'itemlist');
-define('DB_USER','root');
-define('DB_PASSWORD','');
+define('DB_HOST', 'eu-cdbr-west-01.cleardb.com');
+define('DB_NAME', 'heroku_1ed81913353afff');
+define('DB_USER','b16f3dd482fc76');
+define('DB_PASSWORD','d96171c8');
 
         $con=mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD) or die("Failed to connect to MySQL: " .     mysqli_error());
         $db=mysqli_select_db($con,DB_NAME) or die("Failed to connect to MySQL: " . mysqli_error());
